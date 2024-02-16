e8534639aa60d364bc3bf8c75848ec646a68803b
Julian Ewers-Peters <ewerspej@tcd.ie>
2024-02-16 12:12:49 +0100

    file-scoped namespace for AppShell

--- RouteGeneratorSample/AppShell.xaml.cs
+++ RouteGeneratorSample/AppShell.xaml.cs
@@ -1,21 +1,20 @@
 ﻿using System.Diagnostics;
 
-namespace RouteGeneratorSample
+namespace RouteGeneratorSample;
+
+public partial class AppShell : Shell
 {
-    public partial class AppShell : Shell
+    public AppShell()
     {
-        public AppShell()
+        InitializeComponent();
+
+        Routing.RegisterRoute(Routes.SomeOtherRoute, typeof(MainPage));
+
+        foreach (var route in Routes.RouteTypeMap)
         {
-            InitializeComponent();
+            Routing.RegisterRoute(route.Key, route.Value);
 
-            Routing.RegisterRoute(Routes.SomeOtherRoute, typeof(MainPage));
-
-            foreach (var route in Routes.RouteTypeMap)
-            {
-                Routing.RegisterRoute(route.Key, route.Value);
-
-                Debug.WriteLine($"{route.Key}: {route.Value}");
-            }
+            Debug.WriteLine($"{route.Key}: {route.Value}");
         }
     }
 }
